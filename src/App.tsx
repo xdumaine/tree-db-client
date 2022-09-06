c33453f4fb5488a69a35e10263e8b4e034619767
Xander Dumaine <xander.dumaine@lifeomic.com>
2022-09-06 12:05:34 -0400

    step-0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useGenera } from "./hooks.ts/useGenera";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const genera = useGenera();
+  return <pre>{JSON.stringify(genera, null, 2)}</pre>;
 }
 
 export default App;
